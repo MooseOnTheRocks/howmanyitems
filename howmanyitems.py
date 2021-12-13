f57f4aefec583f1ebb25c4c4442151af46667356
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-12-13 03:50:50 -0800

    Search recipes by name using regex

--- howmanyitems.py
+++ howmanyitems.py
@@ -1,5 +1,25 @@
+# howmanyitems.py
+# by MooseOnTheRocks
+#
+# Simple Python interface for Minecraft recipes.
+# Capabilities:
+#   -- Currently, focus on crafting table recipes (shaped and shapeless).
+#   -- Search for recipes with resulting item name.
+#   -- Display total base materials required.
+#   -- Decompose and display item into tree(s) of recipes.
+#       -- Each tree represents a unique way to craft that item.
+#
+# Requirements:
+#   // Load recipes from JSON data
+#   // Filter craftable recipes
+#   -- Search recipe by resulting item name
+#   -- Compute recipe paths for items
+#   -- Compute summary of base materials required
+
 import os
 import json
+import pprint
+import re
 
 from recipecraftingshaped import *
 
@@ -23,12 +43,22 @@ def pred_type(expected_type):
     return pred_key("type", expected_type)
 
 
+def recipes_by_name(recipes, item_name):
+    return [r for r in recipes if r.result_item_name == item_name]
+
+
+def recipes_by_re(recipes, regex):
+    return [r for r in recipes if re.search(regex, r.result_item_name) is not None]
+
+
 def main():
     recipes_json = load_recipes_json()
     shaped_crafting_recipes_json = filter(pred_type("minecraft:crafting_shaped"), recipes_json)
     shaped_crafting_recipes = list(map(RecipeCraftingShaped.from_json, shaped_crafting_recipes_json))
-    print(len(shaped_crafting_recipes))
-    print(shaped_crafting_recipes[0])
+    print("Search recipes by name:")
+    name_query = input(">>> ")
+    by_re = recipes_by_re(shaped_crafting_recipes, name_query)
+    pprint.pprint(by_re)
 
 
 if __name__ == "__main__":
--- recipecraftingshaped.py
+++ recipecraftingshaped.py
@@ -1,9 +1,11 @@
 class RecipeCraftingShaped:
     TYPE = "minecraft:crafting_shaped"
 
-    def __init__(self, result, key, pattern):
+    def __init__(self, result_item_name, result_count, key, pattern):
         # Resulting minecraft item.
-        self.result = result
+        self.result_item_name = result_item_name
+        # Quantity received from crafting
+        self.result_count = result_count
         # A map from characters in pattern to items in minecraft.
         self.key = key
         # An array of 0 to 3 strings each ranging from 0 to 3 chars.
@@ -14,7 +16,10 @@ class RecipeCraftingShaped:
     def from_json(obj):
         assert(obj["type"] == RecipeCraftingShaped.TYPE)
         result, key, pattern = map(lambda k: obj[k], ["result", "key", "pattern"])
-        return RecipeCraftingShaped(result, key, pattern)
+        result_count = result["count"] if "count" in result else 1
+        result_item_namespace, result_item_name = result["item"].split(":")
+
+        return RecipeCraftingShaped(result_item_name, result_count, key, pattern)
 
     def __repr__(self):
-        return f"RecipeCraftingShaped({self.result}, {self.key}, {self.pattern})"
+        return f"RecipeCraftingShaped({self.result_item_name}, {self.result_count}, {self.key}, {self.pattern})"
